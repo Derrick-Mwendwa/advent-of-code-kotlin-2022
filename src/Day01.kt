adde225ac4f65281ac588a5e1dc47f06470fbcd6
Derrick Mwendwa <derekmwendwa14@gmail.com>
2022-12-04 03:37:39 +0300

    Day 01 (Updated) Fixed a bug :) -> Where the last item in the text was not considered

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,32 +1,25 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        val calories = mutableListOf<Int>()
-        var sum = 0
-        input.forEach {
-            sum = if (it.isNotBlank()) sum + it.toInt() else {
-                calories.add(sum)
-                0
+        val calories = input.joinToString("\n").split("\n\n").map {
+            it.lines().sumOf { line ->
+                line.toInt()
             }
         }
         return calories.max()
     }
 
     fun part2(input: List<String>): Int {
-        val calories = mutableListOf<Int>()
-        var sum = 0
-        input.forEach {
-            sum = if (it.isNotBlank()) sum + it.toInt() else {
-                calories.add(sum)
-                0
+        val calories = input.joinToString("\n").split("\n\n").map {
+            it.lines().sumOf { line ->
+                line.toInt()
             }
-        }
-        calories.sortDescending()
+        }.sortedDescending()
         return calories[0] + calories[1] + calories[2]
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
