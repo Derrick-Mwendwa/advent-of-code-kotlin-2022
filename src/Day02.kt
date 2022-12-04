942bea595ddc73e967168f38ffd28edc0a74d6f3
Derrick Mwendwa <derekmwendwa14@gmail.com>
2022-12-04 05:32:38 +0300

    Day 02

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,27 +1,43 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        val calories = input.joinToString("\n").split("\n\n").map {
-            it.lines().sumOf { line ->
-                line.toInt()
-            }
+        // Assuming that we trust the input
+        fun scoreOfShape(shape: Char) = when (shape) {
+            'X' -> 1
+            'Y' -> 2
+            else -> 3
         }
-        return calories.max()
+
+        fun score(opponent: Char, player: Char) = when (opponent to player) {
+            'A' to 'Y', 'C' to 'X', 'B' to 'Z' -> 6
+            'A' to 'X', 'B' to 'Y', 'C' to 'Z' -> 3
+            else -> 0
+        }
+        return input.sumOf { scoreOfShape(it[2]) + score(it[0], it[2]) }
     }
 
     fun part2(input: List<String>): Int {
-        val calories = input.joinToString("\n").split("\n\n").map {
-            it.lines().sumOf { line ->
-                line.toInt()
+        fun scoreOfShape(result: Pair<Char, Int>) = result.second + when (result.first) {
+            'X' -> 1
+            'Y' -> 2
+            else -> 3
+        }
+
+        fun getOutcome(opponent: Char, outcome: Char): Pair<Char,Int> {
+            val ch = when (outcome to opponent) {
+                'X' to 'A', 'Y' to 'C', 'Z' to 'B' -> 'Z'
+                'X' to 'B', 'Y' to 'A', 'Z' to 'C' -> 'X'
+                else -> 'Y'
             }
-        }.sortedDescending()
-        return calories[0] + calories[1] + calories[2]
+            return ch to (outcome - 'X') * 3
+        }
+        return input.sumOf { scoreOfShape(getOutcome(it[0], it[2])) }
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part2(testInput) == 45000)
+    val testInput = readInput("Day02_test")
+    check(part2(testInput) == 12)
 
-    val input = readInput("Day01")
+    val input = readInput("Day02")
     println(part1(input))
     println(part2(input))
 }
