2cf9815f085ca360b27a048b5d7ec719a100543c
Derrick Mwendwa <derekmwendwa14@gmail.com>
2022-12-04 17:11:08 +0300

    Day 03 implemented

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,15 +1,25 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        TODO()
+        fun Char.getPriority() = this.code - if (this.isLowerCase()) 96 else 38
+        return input.sumOf {
+            (it.substring(0, it.length / 2).toSet() intersect it.substring(it.length / 2).toSet())
+                .single()
+                .getPriority()
+        }
     }
 
     fun part2(input: List<String>): Int {
-        TODO()
+        fun Char.getPriority() = this.code - if (this.isLowerCase()) 96 else 38
+        return input.chunked(3).sumOf {
+            (it[0].toSet() intersect it[1].toSet() intersect it[2].toSet())
+                .single()
+                .getPriority()
+        }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
-    check(part1(testInput) == 157)
+    check(part2(testInput) == 70)
 
     val input = readInput("Day03")
     println(part1(input))
